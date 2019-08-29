1adb9904064d2f45db0419492365c392cebd6c95
Hayden Woolstenhulme <hayd1234@gmail.com>
2019-08-29 14:52:56 -0600

    surprise

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/h7876/caginator
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/mtslzr/pokeapi-go v1.3.0
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"github.com/h7876/caginator/internal/response"
+	"github.com/mtslzr/pokeapi-go"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -18,7 +19,7 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		payload := response.Ret{
-			Name: "Hayden",
+			Name: "Charlie",
 		}
 		bytePayload, err := json.MarshalIndent(payload, "", "  ")
 		_ = err
@@ -27,6 +28,14 @@ func main() {
 		// title := vars["title"]
 		// page := vars["page"]
 		// fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+	r.HandleFunc("/pokedex", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		g, _ := pokeapi.Resource("pokemon")
+		w.Header().Set("Content-Type", "application/json")
+		pokeByte, err:= json.MarshalIndent(g, "", "  ")
+		_= err
+		w.Write(pokeByte)
 	})
 	http.ListenAndServe(":8080", r)
 }
